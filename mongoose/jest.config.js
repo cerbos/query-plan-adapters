2201c50aa612a301849ac8b7676f8cfc95bf4a8f
Alex Olivier <alex@alexolivier.me>
2025-10-07 21:38:27 +0100

    fix up
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- mongoose/jest.config.js
+++ mongoose/jest.config.js
@@ -1,17 +1,20 @@
+const esModules = ["uuid"].join("|");
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
   extensionsToTreatAsEsm: [".ts"],
   transform: {
-    "^.+\\.tsx?$": [
+    "^.+\\.[tj]sx?$": [
       "ts-jest",
       {
         useESM: true,
-        tsconfig: "./tsconfig.json",
+        tsconfig: "./tsconfig.jest.json",
       },
     ],
   },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
