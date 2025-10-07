cf79b75a676f9a8204a852f9837a9383b9892388
Alex Olivier <alex@alexolivier.me>
2025-10-07 21:25:50 +0100

    wip
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- mongoose/jest.config.js
+++ mongoose/jest.config.js
@@ -1,5 +1,18 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.json",
+      },
+    ],
+  },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
