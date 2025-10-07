7b2e6575ceb4910ec06ba13afbec69a94f08ae3c
Alex Olivier <alex@alexolivier.me>
2025-10-07 21:40:22 +0100

    fix prisma tests
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- prisma/jest.config.js
+++ prisma/jest.config.js
@@ -1,5 +1,21 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const esModules = ["uuid"].join("|");
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+        tsconfig: "./tsconfig.jest.json",
+      },
+    ],
+  },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
 };
