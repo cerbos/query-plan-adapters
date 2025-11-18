42790b5a214336a6c71c89618522e336a812bb41
Alex Olivier <alex@alexolivier.me>
2025-11-18 10:11:41 +0000

    fix up build
    Signed-off-by: Alex Olivier <alex@alexolivier.me>

--- drizzle/jest.config.js
+++ drizzle/jest.config.js
@@ -1,21 +1,20 @@
+const esModules = ["uuid"].join("|");
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
-  testMatch: ["**/src/**/*.test.ts"],
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts", "!src/**/*.test.ts"],
   extensionsToTreatAsEsm: [".ts"],
-  transformIgnorePatterns: ["node_modules/(?!(uuid)/)"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
   transform: {
     "^.+\\.[tj]sx?$": [
       "ts-jest",
       {
         useESM: true,
-        tsconfig: "./tsconfig.json",
       },
     ],
+  },
+  transformIgnorePatterns: [`node_modules/(?!(${esModules})/)`],
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
 };
